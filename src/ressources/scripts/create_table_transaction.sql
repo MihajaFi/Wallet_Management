ed0bc8bde4767c41fa198b58a1180020b767eb69
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-06 14:01:48 +0300

    feat: a new column name

--- src/ressources/scripts/create_table_transaction.sql
+++ src/ressources/scripts/create_table_transaction.sql
@@ -1,14 +1,15 @@
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'TransactionType') THEN
+        CREATE TYPE TransactionType AS ENUM ('DEBIT', 'CREDIT');
+    END IF;
+END $$;
 
--- Script pour créer la table TransactionType
-CREATE TYPE TransactionType AS ENUM ('DEBIT', 'CREDIT');
-
--- Script pour créer la table Transaction
 CREATE TABLE IF NOT EXISTS Transaction (
-    transactionId SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL,
-    description VARCHAR(255),
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(255),
     amount DOUBLE PRECISION NOT NULL,
-    type TransactionType NOT NULL,
-    accountId VARCHAR(50) REFERENCES Account(accountId)
+    date TIMESTAMP NOT NULL,
+    type TransactionType NOT NULL
 );
 
