9119aefbc8eab9157721e388fa21b811bf92e813
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-06 13:59:22 +0300

    feat: a new column name

--- src/ressources/scripts/create_table_account.sql
+++ src/ressources/scripts/create_table_account.sql
@@ -1,11 +1,19 @@
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'AccountType') THEN
+        CREATE TYPE AccountType AS ENUM ('BANK', 'MM', 'CASH', 'SPECIES');
+    END IF;
+END $$;
 
 -- Script pour créer la table Account
 CREATE TABLE IF NOT EXISTS Account (
-    accountId VARCHAR(50) PRIMARY KEY,
-    balance DOUBLE PRECISION NOT NULL,
-    accountName VARCHAR(50) NOT NULL ,
-    currencyCode VARCHAR(3) REFERENCES Currency(currencyCode),
-    CONSTRAINT positiveBalance CHECK (balance >= 0)
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL ,
+    balance DECIMAL(18 , 5) NOT NULL,
+    id_currency INT REFERENCES Currency(id),
+    type AccountType NOT NULL,
+    CONSTRAINT positiveBalance CHECK (balance >= 0),
+    id_transaction INT REFERENCES Transaction(id)
 );
 
 INSERT INTO Account (accountId ,balance ,accountName , currencyCode)
