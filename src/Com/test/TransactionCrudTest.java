0c1ac5596fb694bd44833762576c1b411a807299
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-07 11:23:01 +0300

    feat: add a test for testing the first function

--- src/Com/test/TransactionCrudTest.java
+++ src/Com/test/TransactionCrudTest.java
@@ -12,25 +12,10 @@ import java.util.List;
 public class TransactionCrudTest {
     public static void TransactionTest(){
         LocalDateTime updatedDate = LocalDateTime.now() ;
-        BigDecimal value = new BigDecimal("455540") ;
-        Transaction transaction = new Transaction(123456 , "Cadeaux noel" ,value , updatedDate , TransactionType.DEBIT , "12345dsdf") ;
-        Transaction secondTransaction = new Transaction(789101 , "Depot Telma" ,value,updatedDate,TransactionType.CREDIT,"azer1244") ;
-        TransactionCrudOperations allTransactions = new TransactionCrudOperations();
-        System.out.println("The list of all transactions : ");
-        allTransactions.findAll() ;
-        List<Transaction> transactions = new ArrayList<>() ;
-        transactions.add(transaction) ;
-        transactions.add(secondTransaction) ;
-        List<Transaction> savedTransactions = allTransactions.saveAll(transactions) ;
-        System.out.println("Saved transactions : ");
-        for (Transaction transaction1 : savedTransactions){
-            System.out.println(transaction1);
-        }
-
-        transaction.setAmount(value);
-        System.out.println("Transactions modified :");
-        allTransactions.update(transaction) ;
-        System.out.println(transaction);
+        BigDecimal value = new BigDecimal("45000") ;
+        Transaction transaction = new Transaction(12345678 , "Cadeaux noel" ,value , updatedDate , TransactionType.DEBIT , "12345678") ;
+        TransactionCrudOperations test = new TransactionCrudOperations() ;
+        test.save(transaction) ;
 
     }
 }
