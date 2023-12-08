fb0f20ca1067f8ebc00d9f4781e80979e9eb029e
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-08 07:13:14 +0300

    feat : simulating a test for transaction of an account

--- src/Com/test/TransactionCrudTest.java
+++ src/Com/test/TransactionCrudTest.java
@@ -1,7 +1,8 @@
 package Com.test;
 
-import models.Transaction;
-import models.TransactionType;
+import models.*;
+import repository.AccountCrudOperations;
+import repository.HistoryCrudOperation;
 import repository.TransactionCrudOperations;
 
 import java.math.BigDecimal;
@@ -11,11 +12,22 @@ import java.util.List;
 
 public class TransactionCrudTest {
     public static void TransactionTest(){
+        // Test all method
         LocalDateTime updatedDate = LocalDateTime.now() ;
-        BigDecimal value = new BigDecimal("45000") ;
-        Transaction transaction = new Transaction(12345678 , "Cadeaux noel" ,value , updatedDate , TransactionType.DEBIT , "12345678") ;
+        BigDecimal value1 = new BigDecimal("0") ;
+        Account firstAccount = new Account("Fifaliana" , "General" ,value1, updatedDate ,1 , AccountType.BANK ) ;
+        AccountCrudOperations ac = new AccountCrudOperations() ;
+        ac.save(firstAccount) ;
+
+        BigDecimal value = new BigDecimal("50000") ;
+        Transaction transaction = new Transaction(2, "Depot Account" ,value , updatedDate , TransactionType.CREDIT , "Fifaliana") ;
         TransactionCrudOperations test = new TransactionCrudOperations() ;
         test.save(transaction) ;
 
+        LocalDateTime startDate = LocalDateTime.of(2023, 12, 8, 0, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2023, 12, 8, 23, 59, 59);
+
+        List<BalanceHistory> balanceHistories = HistoryCrudOperation.getBalanceHistory("Fifaliana" , startDate , endDate);
+        System.out.println(balanceHistories);
     }
 }
