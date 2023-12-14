a381829a7038798e3e90a7cd0b6f7a7d56e262fd
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-14 18:26:44 +0300

    feat: script for inserting all categories in budgetBakers

--- src/ressources/scripts/create_table_transaction_category.sql
+++ src/ressources/scripts/create_table_transaction_category.sql
@@ -28,3 +28,47 @@ WHERE NOT EXISTS (
     WHERE id = 'Cat3' AND name = 'Salary'
 )
 
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat4' , 'Food', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat4' AND name = 'Food'
+)
+
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat5' , 'Shopping ', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat5' AND name = 'Shopping '
+)
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat6' , 'Accommodation', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat6' AND name = 'Accommodation'
+)
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat7' , 'Transport', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat7' AND name = 'Transport'
+)
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat8' , 'Investments', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat8' AND name = 'Investments'
+)
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat9' , 'beverages', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat9' AND name = 'beverages'
+)
+INSERT INTO Category (id , name , is_income , is_expense)
+SELECT 'Cat10' , 'online shops', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat10' AND name = 'online shops'
+)
+
