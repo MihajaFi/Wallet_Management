91ceabfe11d3be0c0f46b6d7d4b5dbf32d5a4722
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-14 14:28:59 +0300

    feat: script for table transaction category to find out the nature of each transaction

---
+++ src/ressources/scripts/create_table_transaction_category.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS Category (
+    id VARCHAR(100) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    is_income BOOLEAN NOT NULL,
+    is_expense BOOLEAN NOT NULL
+);
+
+
+INSERT INTO Category (id, name, is_income, is_expense)
+SELECT 'Cat1', 'Restaurant', true, false
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat1' AND name = 'Restaurant'
+);
+
+
+INSERT INTO Category (id, name, is_income, is_expense)
+SELECT 'Cat2', 'Telephone', false, true
+WHERE NOT EXISTS (
+    SELECT 1 FROM Category
+    WHERE id = 'Cat2' AND name = 'Telephone'
+);
+
