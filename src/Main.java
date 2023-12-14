67714c1d8a5bf8c918ac333a5ed8f8da2596f503
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-14 17:41:10 +0300

    feat: add test to transfer money

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,24 @@ import services.MoneyTransfer;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
-//     TransactionCrudTest.TransactionTest();
-        BigDecimal value1 = new BigDecimal("45000") ;
-        MoneyTransfer.transferMoney("Koto" , "Fifaliana" ,value1 );
+
+        BigDecimal value1 = new BigDecimal("1000") ;
+
+        MoneyTransfer.transferMoney("Acc1" , "Acc2" ,value1 ,"Cat1", "Cat3" );
+
+
+        int senderTransactionId = MoneyTransfer.getLatestTransactionId("Acc1") ;
+        int receiverTransactionId = MoneyTransfer.getLatestTransactionId("Acc2");
+        MoneyTransfer.recordTransferHistory(senderTransactionId , receiverTransactionId);
+        LocalDateTime startDate = LocalDateTime.of(2023, 12, 14, 17, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2023, 12, 14, 17, 59, 59);
+        MoneyTransfer.displayTransferHistoryInDateRange(startDate ,endDate);
+
     }
 }
