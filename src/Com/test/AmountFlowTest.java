0a61230e7f385f924054544919171b7b60a84ca2
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-15 13:40:37 +0300

    feat: test for a sum of amount in getAmountFlowById

--- src/Com/test/AmountFlowTest.java
+++ src/Com/test/AmountFlowTest.java
@@ -1,8 +1,11 @@
 package Com.test;
 
+import repository.CustomTransactionOperations;
+import repository.TransactionCrudOperations;
 import services.AmountFlowMap;
 import utils.AmountFlowResult;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class AmountFlowTest {
@@ -14,4 +17,24 @@ public class AmountFlowTest {
         System.out.println("Total debit : " + result.getTotalDebit());
 
     }
+
+    public static void AmountTest() {
+        TransactionCrudOperations transactionCrudOperations = new TransactionCrudOperations();
+        CustomTransactionOperations customTransactionOperations = new CustomTransactionOperations(transactionCrudOperations);
+
+        // Supposons que les dates de début et de fin soient définies comme suit
+        LocalDateTime startDate = LocalDateTime.of(2023, 12, 14, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2023, 12, 15, 23, 59);
+
+        // Supposons que accountId soit une valeur valide
+        String accountId = "Fifaliana";
+
+        // Appelez la méthode getAmountFlowByAccountId
+        BigDecimal[] result = customTransactionOperations.getAmountFlowByAccountId(accountId, startDate, endDate);
+
+        // Affichez les résultats
+        System.out.println("Total Credit: " + result[0]);
+        System.out.println("Total Debit: " + result[1]);
+    }
+
 }
