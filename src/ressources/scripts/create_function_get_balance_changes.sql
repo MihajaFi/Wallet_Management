2c60519e70248709f9901f4cd97ca6bbd62998ac
RAVONDRAINIBE Geraldo <hei.ravo@gmail.com>
2023-12-15 08:36:39 +0300

    feat: fix a function get balance changes

--- src/ressources/scripts/create_function_get_balance_changes.sql
+++ src/ressources/scripts/create_function_get_balance_changes.sql
@@ -1,20 +1,21 @@
-CREATE OR REPLACE FUNCTION get_balance_changes(
+CREATE OR REPLACE FUNCTION get_amount_flow(
     account_id VARCHAR(50),
     start_date TIMESTAMP,
     end_date TIMESTAMP
-) RETURNS DECIMAL(18, 5) AS $$
-DECLARE
-total_balance DECIMAL(18, 5) := 0;
+) RETURNS TABLE (
+    total_credit DOUBLE PRECISION,
+    total_debit DOUBLE PRECISION
+)
+AS $$
 BEGIN
+RETURN QUERY
 SELECT
-        COALESCE(SUM(CASE WHEN type = 'CREDIT' THEN amount ELSE 0 END), 0) -
-        COALESCE(SUM(CASE WHEN type = 'DEBIT' THEN amount ELSE 0 END), 0)
-INTO total_balance
-FROM Transaction
-WHERE id_account = account_id AND date BETWEEN start_date AND end_date;
-
-RETURN total_balance;
+    COALESCE(SUM(CASE WHEN t.type = 'CREDIT' THEN t.amount END), 0) AS total_credit,
+    COALESCE(SUM(CASE WHEN t.type = 'DEBIT' THEN t.amount END), 0) AS total_debit
+FROM
+    Account a
+        LEFT JOIN Transaction t ON a.id = t.id_account AND t.date BETWEEN start_date AND end_date
+WHERE
+        a.id = account_id;
 END;
 $$ LANGUAGE plpgsql;
-
-SELECT get_balance_changes('votre_id_de_compte', '2023-01-01'::TIMESTAMP, '2023-12-31'::TIMESTAMP);
