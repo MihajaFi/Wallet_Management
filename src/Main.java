834a5c123f6b92bc2cef61778caeae536cffc21a
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-15 08:47:22 +0300

    feat: run the test of the mapping data

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Com.test.AmountFlowTest;
+import Com.test.CategorySumTest;
 import Com.test.TransactionCrudTest;
 import services.MoneyTransfer;
 
@@ -10,17 +12,20 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        BigDecimal value1 = new BigDecimal("1000") ;
+//        BigDecimal value1 = new BigDecimal("1000") ;
+//
+//        MoneyTransfer.transferMoney("Acc1" , "Acc2" ,value1 ,"Cat1", "Cat3" );
+//
+//
+//        int senderTransactionId = MoneyTransfer.getLatestTransactionId("Acc1") ;
+//        int receiverTransactionId = MoneyTransfer.getLatestTransactionId("Acc2");
+//        MoneyTransfer.recordTransferHistory(senderTransactionId , receiverTransactionId);
+//        LocalDateTime startDate = LocalDateTime.of(2023, 12, 14, 17, 0, 0);
+//        LocalDateTime endDate = LocalDateTime.of(2023, 12, 14, 17, 59, 59);
+//        MoneyTransfer.displayTransferHistoryInDateRange(startDate ,endDate);
 
-        MoneyTransfer.transferMoney("Acc1" , "Acc2" ,value1 ,"Cat1", "Cat3" );
-
-
-        int senderTransactionId = MoneyTransfer.getLatestTransactionId("Acc1") ;
-        int receiverTransactionId = MoneyTransfer.getLatestTransactionId("Acc2");
-        MoneyTransfer.recordTransferHistory(senderTransactionId , receiverTransactionId);
-        LocalDateTime startDate = LocalDateTime.of(2023, 12, 14, 17, 0, 0);
-        LocalDateTime endDate = LocalDateTime.of(2023, 12, 14, 17, 59, 59);
-        MoneyTransfer.displayTransferHistoryInDateRange(startDate ,endDate);
+        AmountFlowTest.FlowTest();
+        CategorySumTest.sumCategoryTest();
 
     }
 }
